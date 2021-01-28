114a0a7a3df07c18fd502b59ccf9385eb359b2fe
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-01-28 22:33:39 +0530

    Basic Intro Problems

---
+++ Simple_intrest.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int P; 
+    int T;
+    int R;
+    float SI;
+    // int/int = int
+    // float/int = float10
+    // int/float = float
+
+    cin>>P>>R>>T;
+
+    SI = (P*R*T)/100.0;
+    cout<<SI<<endl;
+}
