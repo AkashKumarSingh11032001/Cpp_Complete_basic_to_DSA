ca5471ce5aa94e4df1ba3993c8b5609d04e9b5d0
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-02-02 22:19:11 +0530

    Arrays 2D

--- 2DArrays_SprialPrint.cpp
+++ 2DArrays_SprialPrint.cpp
@@ -16,19 +16,62 @@ int main()
         {
             array[i][j] = val;
             val++;
-
         }
-        cout<<endl;
+        cout << endl;
     }
 
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
-            cout << array[i][j]<<" ";
+            cout << array[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
     //Spiral print
+
+    int start_row = 0;
+    int start_col = 0;
+    int end_row = row-1;
+    int end_col = col-1;
+
+    while (start_row <= end_row and start_col <= end_row)
+    {
+        //first row{top}
+        for (int i = start_col; i <= end_col; i++)
+        {
+            cout << array[start_row][i] << " ";
+        }
+        start_row++;
+
+        //end row(left)
+        for (int i = start_row; i <= end_row; i++)
+        {
+            cout << array[i][end_col] << " ";
+        }
+        end_col--;
+
+        //bottom row(down)
+        if (end_row > start_row)
+        {
+            for (int i = end_row; i >= start_col; i--)
+            {
+                cout << array[end_row][i] << " ";
+            }
+            end_row--;
+        }
+
+        //start row(right)
+        if (end_col > start_col)
+        {
+            for (int i = end_row; i >= start_row; i--)
+            {
+                cout << array[i][start_col] << " ";
+            }
+            start_col++;
+        }
+    }
+
+    return 0;
 }
